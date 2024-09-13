c95581464d6b55e9c73b79d0ab29025301883b8e
Meirong <meirongdev@gmail.com>
2024-09-13 12:50:50 +0800

    feat: implement createPost by tanstack query

--- src/api/posts.js
+++ src/api/posts.js
@@ -5,3 +5,18 @@ export const getPosts = async (queryParams) => {
   )
   return await res.json()
 }
+
+export const createPost = async ({ title, content, author }) => {
+  console.log('create post:', title, content, author)
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title, content, author }),
+  })
+  if (!res.ok) {
+    throw new Error('Failed to create post', res)
+  }
+  return await res.json()
+}
--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,22 +1,69 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useState } from 'react'
+import { createPost } from '../api/posts.js'
+
 export function CreatePost() {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [content, setContent] = useState('')
+  const queryClient = useQueryClient()
+  const createPostMutation = useMutation({
+    mutationFn: () => createPost({ title, content, author }),
+    onSuccess: () => queryClient.invalidateQueries('posts'),
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    createPostMutation.mutate()
+  }
+
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={handleSubmit}>
       <fieldset>
         <legend>Create a new post</legend>
       </fieldset>
-      <div>
-        <label htmlFor='create-title'>Title: </label>
-        <input type='text' name='create-title' id='create-title' />
-      </div>
-      <div>
-        <label htmlFor='create-author'>Author: </label>
-        <input type='text' name='create-author' id='create-author' />
-      </div>
-      <div>
-        <label htmlFor='create-content'>Content: </label>
-        <textarea id='create-content' name='create-content' />
-      </div>
-      <input type='submit' value='Create' />
+      <label htmlFor='create-title'>
+        Title
+        <input
+          type='text'
+          name='create-title'
+          id='create-title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+      </label>
+      <label htmlFor='create-author'>
+        Author
+        <input
+          type='text'
+          name='create-author'
+          id='create-author'
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+      </label>
+      <label htmlFor='create-content'>
+        Content
+        <textarea
+          id='create-content'
+          name='create-content'
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+      </label>
+      <input
+        type='submit'
+        role='button'
+        value={createPostMutation.isPending ? 'Creating...' : 'Create'}
+        disabled={!title || createPostMutation.isPending}
+      />
+      {createPostMutation.isSuccess ? (
+        <>
+          <br />
+          created successfully!
+        </>
+      ) : null}
     </form>
   )
 }
